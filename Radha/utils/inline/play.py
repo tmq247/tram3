835bad5ab08bdd78989645b0f43eed01b28ee8dd
Eren Yeager <theerenyeagerx@gmail.com>
2025-08-11 10:55:54 +0530

    by @TheErenYeagerx

--- Radha/utils/inline/play.py
+++ Radha/utils/inline/play.py
@@ -1,6 +1,7 @@
 import math
-from config import SUPPORT_CHAT, OWNER_USERNAME
+
 from pyrogram.types import InlineKeyboardButton
+
 from Radha.utils.formatters import time_to_seconds
 
 
@@ -31,72 +32,52 @@ def stream_markup_timer(_, chat_id, played, dur):
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
+
     if 0 < umm <= 10:
-        bar = "▰▱▱▱▱▱▱▱▰"
-    elif 10 < umm < 20:
-        bar = "▰▱▱▱▱▱▱▱▰"
-    elif 20 <= umm < 30:
-        bar = "▰▱▱▱▱▱▱▱▰"
-    elif 30 <= umm < 40:
-        bar = "▰▱▱▱▱▱▱▱▰"
-    elif 40 <= umm < 50:
-        bar = "▰▱▱▱▱▱▱▱▰"
-    elif 50 <= umm < 60:
-        bar = "▰▱▱▱▱▱▱▱▰"
-    elif 60 <= umm < 70:
-        bar = "▰▱▱▱▱▱▱▱▰"
-    elif 70 <= umm < 80:
-        bar = "▰▱▱▱▱▱▱▱▰"
-    elif 80 <= umm < 95:
-        bar = "▰▱▱▱▱▱▱▱▰"
+        bar = "▁▂▂▃▄▃▂▂▁"
+    elif 10 < umm <= 20:
+        bar = "▂▃▄▅▅▄▃▂▁"
+    elif 20 < umm <= 30:
+        bar = "▃▄▅▆▆▅▄▃▂"
+    elif 30 < umm <= 40:
+        bar = "▄▅▆▇▇▆▅▄▃"
+    elif 40 < umm <= 50:
+        bar = "▅▆▇██▇▆▅▄"
+    elif 50 < umm <= 60:
+        bar = "▆▇███▇▆▅▄"
+    elif 60 < umm <= 70:
+        bar = "▇████▇▆▅▄"
+    elif 70 < umm <= 80:
+        bar = "█████▇▆▅▄"
+    elif 80 < umm <= 90:
+        bar = "▇████▆▅▄▃"
+    elif 90 < umm <= 100:
+        bar = "▆▇██▅▄▃▂▁"
     else:
-        bar = "▰▱▱▱▱▱▱▱▰"
+        bar = "▆▇▅▄█▃▂▁"
+
     buttons = [
         [
             InlineKeyboardButton(
                 text=f"{played} {bar} {dur}",
                 callback_data="GetTimer",
-            )
+            ), 
         ],
         [
-            InlineKeyboardButton(text="◁", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="☊", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton(
-
-                text="✙ 𝐀ᴅᴅ 𝐌є",
-
-                url=f"https://t.me/Sung_Jin_Woo_Bot?startgroup=true",
-
-            ),
-        ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
-    ]
+            InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            InlineKeyboardButton(text="ᐅ", callback_data=f"ADMIN Resume|{chat_id}")],
+   ]
     return buttons
 
 
 def stream_markup(_, chat_id):
     buttons = [
         [
-            InlineKeyboardButton(text="◁", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="☊", callback_data=f"ADMIN Pause|{chat_id}"),
-            InlineKeyboardButton(text="▷", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton(
-
-                text="✙ 𝐀ᴅᴅ 𝐌є",
-
-                url=f"https://t.me/Sung_Jin_Woo_Bot?startgroup=true",
-
-            ),
-        ],
-        [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
-    ]
+           InlineKeyboardButton(text="❚❚", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close"),
+            InlineKeyboardButton(text="ᐅ", callback_data=f"ADMIN Resume|{chat_id}")],
+   ]
     return buttons
 
 
@@ -105,11 +86,11 @@ def playlist_markup(_, videoid, user_id, ptype, channel, fplay):
         [
             InlineKeyboardButton(
                 text=_["P_B_1"],
-                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
+                callback_data=f"RadhaPlaylists {videoid}|{user_id}|{ptype}|a|{channel}|{fplay}",
             ),
             InlineKeyboardButton(
                 text=_["P_B_2"],
-                callback_data=f"AnonyPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
+                callback_data=f"RadhaPlaylists {videoid}|{user_id}|{ptype}|v|{channel}|{fplay}",
             ),
         ],
         [
