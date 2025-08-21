3c165aa1dbf8962e738e429eb82c25d101ff83ee
Eren Yeager <theerenyeagerx@gmail.com>
2025-08-21 14:20:55 +0530

    Update __main__.py

--- Radha/__main__.py
+++ Radha/__main__.py
@@ -1,16 +1,18 @@
 import asyncio
 import importlib
+from typing import Any
 
 from pyrogram import idle
 from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
+from config import BANNED_USERS
 from Radha import LOGGER, app, userbot
 from Radha.core.call import Radha
 from Radha.misc import sudo
 from Radha.plugins import ALL_MODULES
 from Radha.utils.database import get_banned_users, get_gbanned
-from config import BANNED_USERS
+from Radha.core.cookies import save_cookies
 
 
 async def init():
@@ -21,42 +23,40 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER(__name__).error("String Session Not Filled, Please Fill A Pyrogram Session")
+        LOGGER(__name__).error("🧾 String Session Not Filled, Please Fill A Pyrogram Session")
         exit()
     await sudo()
     try:
-        users = await get_gbanned()
-        for user_id in users:
+        for user_id in await get_gbanned():
             BANNED_USERS.add(user_id)
-        users = await get_banned_users()
-        for user_id in users:
+        for user_id in await get_banned_users():
             BANNED_USERS.add(user_id)
-    except:
+    except Exception:
         pass
     await app.start()
-    for all_module in ALL_MODULES:
-        importlib.import_module("Radha.plugins" + all_module)
-    LOGGER("Radha.plugins").info("All features have been successfully loaded")
+    await save_cookies()
+    for module in ALL_MODULES:
+        importlib.import_module(f"Radha.plugins{module}")
+    LOGGER("Radha.plugins").info("🌸 Necessary Modules Imported Successfully.")
     await userbot.start()
     await Radha.start()
     try:
         await Radha.stream_call("https://files.catbox.moe/21umq0.mp4")
     except NoActiveGroupCall:
         LOGGER("Radha").error(
-            "Please Start Your Log Group VoiceChat Or Channel\n\nRadha Bot Stopped"
+            "⭐ Turn on group voice chat and don't put it off otherwise I'll stop working thanks."
         )
         exit()
-    except:
+    except Exception:
         pass
     await Radha.decorators()
-    LOGGER("Radha").info(
-        "Made By @RadhaSprt"
-    )
+    LOGGER("Radha").info("🎶 Radha Music Bot Started Successfully")
     await idle()
     await app.stop()
     await userbot.stop()
-    LOGGER("Radha").info("Radha Bot Stopped")
+    LOGGER("Radha").info("🚫 Stopping Music Music Bot...")
 
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(init())
+    LOGGER("Radha").info("🚫 Stopping Music Bot")
